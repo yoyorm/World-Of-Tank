1d52fde19f6118d53281d356f46c9c4360dbe0d1
yoyorm <99800494+yoyorm@users.noreply.github.com>
2025-12-25 20:25:57 +0800

    [fix] 补充疏漏的文件

---
+++ Assets/Scripts/Manager/MainMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public void OnTestClick() 
+    {
+        Debug.Log("开始游戏");
+        
+        SceneManager.LoadScene("Test");
+    }
+}
